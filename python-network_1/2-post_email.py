b422b926819cb8746f7efa1f0a1a99aa4f12d520
TuranX857 <quliyevturan857@gmail.com>
2026-04-22 18:33:37 +0000

    salam

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Sends a POST request with an email parameter and displays the response body."""
+"""Sends a POST request with an email
+ parameter and displays the response body."""
 
 import sys
 from urllib import request, parse
