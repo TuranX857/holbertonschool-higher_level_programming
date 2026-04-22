6bdaac135556843b8583913e6ce21794da64c4db
TuranX857 <quliyevturan857@gmail.com>
2026-04-22 19:08:08 +0000

    salam

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -38,7 +38,7 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Not Found")
 
 
 def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
